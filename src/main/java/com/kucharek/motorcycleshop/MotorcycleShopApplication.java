df1fa93b0e515c396fadd55c861a6d48ea8aa1fb
pkucharek <piotrkucharek1@gmail.com>
2020-05-12 20:16:47 +0200

    Added initial project files with gitignore

---
+++ src/main/java/com/kucharek/motorcycleshop/MotorcycleShopApplication.java
@@ -0,0 +1,13 @@
+package com.kucharek.motorcycleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorcycleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorcycleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kucharek/motorcycleshop/MotorcycleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kucharek.motorcycleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorcycleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
