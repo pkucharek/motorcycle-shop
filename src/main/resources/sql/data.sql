95c0593e993790fb2962fb70dce368d2e89e4296
pkucharek <piotrkucharek1@gmail.com>
2020-05-13 20:23:52 +0200

    Added sample motorcycles data

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,25 @@
+use motorcycle_shop;
+insert into motorcycle
+	(model, brand, color, type, production_year, cylinders, engine_capacity)
+values 
+	("Yamaha", "Fazer", "black", "standard", 1999, 4, 600);
+    
+insert into motorcycle
+	(model, brand, color, type, production_year, cylinders, engine_capacity)
+values 
+	("Honda", "Cbr", "orange", "sport bike", 2018, 6, 1000);
+    
+insert into motorcycle
+	(model, brand, color, type, production_year, cylinders, engine_capacity)
+values 
+	("Harley-Davidson", "Street Rod", "black", "cruiser", 2020, 6, 800);
+    
+insert into motorcycle
+	(model, brand, color, type, production_year, cylinders, engine_capacity)
+values 
+	("Suzuki", "Gladius", "white-blue", "naked", 2016, 4, 650);
+
+insert into motorcycle
+	(model, brand, color, type, production_year, cylinders, engine_capacity)
+values 
+	("Romet", "ZXT125", "red", "naked", 2020, 4, 125); 
