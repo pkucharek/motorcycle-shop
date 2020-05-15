5f27e5188be7e50a0e095ca29fc467d926a1d751
pkucharek <piotrkucharek1@gmail.com>
2020-05-15 18:07:49 +0200

    Added model attribute to add motorcycles to view

--- src/main/java/com/kucharek/motorcycleshop/controller/HomeController.java
+++ src/main/java/com/kucharek/motorcycleshop/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.kucharek.motorcycleshop.controller;
 
+import com.kucharek.motorcycleshop.data.Motorcycle;
+import com.kucharek.motorcycleshop.service.MotorcycleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private MotorcycleService motorcycleService;
+
     @GetMapping
-    public String homePage() {
+    public String homePage(Model model) {
+        List<Motorcycle> motorcycles = motorcycleService.findAll();
+        model.addAttribute("motorcycles", motorcycles);
         return "home";
     }
 }
