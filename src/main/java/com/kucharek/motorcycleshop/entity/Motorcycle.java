ede376ba1efb06fe46d9540f2d4022cfcd71b10c
pkucharek <piotrkucharek1@gmail.com>
2020-05-15 17:33:07 +0200

    Added Motorcycle entity

---
+++ src/main/java/com/kucharek/motorcycleshop/entity/Motorcycle.java
@@ -0,0 +1,43 @@
+package com.kucharek.motorcycleshop.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Year;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table
+public class Motorcycle {
+
+    @Id
+    @GeneratedValue
+    @Column
+    private long id;
+
+    @Column
+    private String brand;
+
+    @Column
+    private String model;
+
+    @Column
+    private String color;
+
+    @Column
+    private String type;
+
+    @Convert(converter = YearAttributeConverter.class)
+    @Column(name = "production_year")
+    private Year productionYear;
+
+    @Column
+    private byte cylinders;
+
+    @Column
+    private short engineCapacity;
+
+}
