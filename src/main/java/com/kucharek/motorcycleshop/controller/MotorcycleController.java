dab5f200906d5d146a38e623e2c5b0957e943a44
pkucharek <piotrkucharek1@gmail.com>
2020-05-16 12:44:30 +0200

    Added error handling in controller layer

--- src/main/java/com/kucharek/motorcycleshop/controller/MotorcycleController.java
+++ src/main/java/com/kucharek/motorcycleshop/controller/MotorcycleController.java
@@ -5,11 +5,12 @@ import com.kucharek.motorcycleshop.service.MotorcycleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -33,7 +34,10 @@ public class MotorcycleController {
     }
 
     @PostMapping("/save")
-    public String saveMotorcycle(@ModelAttribute("motorcycle") Motorcycle motorcycle) {
+    public String saveMotorcycle(@Valid Motorcycle motorcycle, Errors errors) {
+        if (errors.hasErrors())
+            return "motorcycles/form";
+
         motorcycleService.save(motorcycle);
         return "redirect:/motorcycles/list";
     }
