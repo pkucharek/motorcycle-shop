32ec936ffe3b28736b3a3fd0e06fddd7bd6b2da0
pkucharek <piotrkucharek1@gmail.com>
2020-05-16 12:43:36 +0200

    Added validation to Entity Motorcycle

--- src/main/java/com/kucharek/motorcycleshop/data/Motorcycle.java
+++ src/main/java/com/kucharek/motorcycleshop/data/Motorcycle.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.Year;
 
 @Data
@@ -18,25 +19,37 @@ public class Motorcycle {
     private long id;
 
     @Column
+    @NotBlank(message = "Brand is obligatory")
     private String brand;
 
     @Column
+    @NotBlank(message = "Model is obligatory")
     private String model;
 
     @Column
+    @NotBlank(message = "Color is obligatory")
     private String color;
 
     @Column
+    @NotBlank(message = "Type is obligatory")
     private String type;
 
     @Convert(converter = YearAttributeConverter.class)
     @Column(name = "production_year")
+    @NotNull(message = "Production year is obligatory")
+    @ProductionYearConstraint(message = "Production year should be between 1885 and current year")
     private Year productionYear;
 
     @Column
+    @NotNull(message = "Cylinders are obligatory")
+    @Min(value = 1, message = "Cylinders number should be greater or equal than 1")
+    @Max(value = 12, message = "Cylinders number should be less or equal than 12")
     private byte cylinders;
 
     @Column
+    @NotNull(message = "Engine capacity is obligatory")
+    @Min(value = 1, message = "Engine capacity should be greater or equal than 1")
+    @Max(value = 3000, message = "Engine capacity should be less or equal than 3000")
     private short engineCapacity;
 
 }
