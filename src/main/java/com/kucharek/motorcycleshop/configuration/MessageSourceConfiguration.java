e7e498c03563757dbde5a79e9d1d158ad01df249
pkucharek <piotrkucharek1@gmail.com>
2020-05-16 21:31:15 +0200

    Added resource bundle for yaml message file

--- src/main/java/com/kucharek/motorcycleshop/configuration/MessageSourceConfiguration.java
+++ src/main/java/com/kucharek/motorcycleshop/configuration/MessageSourceConfiguration.java
@@ -1,11 +1,13 @@
 package com.kucharek.motorcycleshop.configuration;
 
+import net.rakugakibox.util.YamlResourceBundle;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 @Configuration
 public class MessageSourceConfiguration {
@@ -15,5 +17,13 @@ public class MessageSourceConfiguration {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         return messageSource;
+    }
+
+    @Bean
+    public ResourceBundle resourceBundle() throws IOException {
+        return ResourceBundle.getBundle(
+                "com.kucharek.motorcycleshop.messages",
+                YamlResourceBundle.Control.INSTANCE
+        );
     }
 }
