be7b3b5b9c453bec0283f96415fe0cef183f4699
pkucharek <piotrkucharek1@gmail.com>
2020-05-17 16:17:49 +0200

    Added spring security config with in memory authentication for testing

---
+++ src/main/java/com/kucharek/motorcycleshop/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.kucharek.motorcycleshop.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("test")
+                    .password("{noop}test")
+                    .authorities("ROLE_USER");
+    }
+
+}
