14e9cf44d78a89112220511048b71f7367e96faa
pkucharek <piotrkucharek1@gmail.com>
2020-05-17 18:02:29 +0200

    Added security for adding motorcycle

--- src/main/java/com/kucharek/motorcycleshop/security/SecurityConfig.java
+++ src/main/java/com/kucharek/motorcycleshop/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kucharek.motorcycleshop.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,4 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorities("ROLE_USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/motorcycles/showFormForAdd")
+                    .hasRole("USER")
+                .antMatchers("/", "/**")
+                    .permitAll();
+    }
 }
