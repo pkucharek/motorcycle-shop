e523e35477d76f87bebe8ac62442c2cebc1881fe
pkucharek <piotrkucharek1@gmail.com>
2020-05-21 21:50:17 +0200

    Modified fetch type in user class

--- src/main/java/com/kucharek/motorcycleshop/data/User.java
+++ src/main/java/com/kucharek/motorcycleshop/data/User.java
@@ -33,7 +33,7 @@ public class User {
     @Column(name = "email")
     private String email;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "users_roles",
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
