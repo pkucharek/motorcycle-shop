8cfa05815da941d72bb81101dda1e9e4238e50ac
pkucharek <piotrkucharek1@gmail.com>
2020-05-21 15:51:26 +0200

    Added custom validation to FormUser entity

--- src/main/java/com/kucharek/motorcycleshop/user/FormUser.java
+++ src/main/java/com/kucharek/motorcycleshop/user/FormUser.java
@@ -1,21 +1,37 @@
 package com.kucharek.motorcycleshop.user;
 
+import com.kucharek.motorcycleshop.user.validation.FieldMatch;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 @NoArgsConstructor
+@FieldMatch.List({
+    @FieldMatch(first = "password", second = "matchingPassword",
+                message = "The password fields must match")
+})
 public class FormUser {
 
+    @NotBlank(message = "{user.notNull}")
     private String userName;
 
+    @NotBlank(message = "{user.notNull}")
     private String password;
 
+    @NotBlank(message = "{user.notNull}")
     private String matchingPassword;
 
+    @NotBlank(message = "{user.notNull}")
     private String firstName;
 
+    @NotBlank(message = "{user.notNull}")
     private String lastName;
 
+    @Pattern(regexp = "^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@" +
+                      "[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$",
+             message = "{user.validEmail}")
     private String email;
 }
