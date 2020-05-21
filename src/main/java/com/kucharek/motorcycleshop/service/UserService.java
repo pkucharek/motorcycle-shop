9d6d7b12eeed9020c6e9b4783768cb6fc030b6ee
pkucharek <piotrkucharek1@gmail.com>
2020-05-21 17:59:23 +0200

    Added UserService

---
+++ src/main/java/com/kucharek/motorcycleshop/service/UserService.java
@@ -0,0 +1,13 @@
+package com.kucharek.motorcycleshop.service;
+
+import com.kucharek.motorcycleshop.data.User;
+import com.kucharek.motorcycleshop.user.FormUser;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService
+        extends UserDetailsService {
+
+    User findByUserName(String userName);
+
+    void save(FormUser formUser);
+}
