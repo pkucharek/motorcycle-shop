44cb6c33b9873b6285e6f6b59b14bcf568889938
pkucharek <--global>
2020-08-09 05:19:13 +0200

    Added method to repository to get only not expired offers

--- src/main/java/com/kucharek/motorcycleshop/data/OfferRepository.java
+++ src/main/java/com/kucharek/motorcycleshop/data/OfferRepository.java
@@ -2,6 +2,10 @@ package com.kucharek.motorcycleshop.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OfferRepository
     extends JpaRepository<Offer, Long> {
+
+    List<Offer> findOffersByExpiredIsFalse();
 }
