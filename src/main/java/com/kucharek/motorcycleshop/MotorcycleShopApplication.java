d777c6a43fd74899b1fbd873f5bd16c5afb1e6fb
pkucharek <--global>
2020-08-13 20:05:57 +0200

    On startup application clears upload-dir and loads sample images

--- src/main/java/com/kucharek/motorcycleshop/MotorcycleShopApplication.java
+++ src/main/java/com/kucharek/motorcycleshop/MotorcycleShopApplication.java
@@ -1,7 +1,10 @@
 package com.kucharek.motorcycleshop;
 
+import com.kucharek.motorcycleshop.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MotorcycleShopApplication {
@@ -10,4 +13,11 @@ public class MotorcycleShopApplication {
 		SpringApplication.run(MotorcycleShopApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
