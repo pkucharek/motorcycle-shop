aadede6590bf7e64de2bfbccd2eb9de9f188b78d
pkucharek <--global>
2020-08-28 21:45:58 +0200

    Changed sample user data

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,36 +1,36 @@
 use motorcycle_shop;
 insert into motorcycle
 	(brand, model, color, type, production_year, cylinders, engine_capacity)
-values 
+values
 	("Yamaha", "Fazer", "czarny", "szosowo-turystyczny", 1999, 4, 600);
-    
+
 insert into motorcycle
 	(brand, model, color, type, production_year, cylinders, engine_capacity)
-values 
+values
 	("Honda", "Cbr", "pomarańczowy", "sportowy", 2018, 6, 1000);
-    
+
 insert into motorcycle
 	(brand, model, color, type, production_year, cylinders, engine_capacity)
-values 
+values
 	("Harley-Davidson", "Street Rod", "czarno-biały", "cruiser", 2020, 6, 800);
-    
+
 insert into motorcycle
 	(brand, model, color, type, production_year, cylinders, engine_capacity)
-values 
+values
 	("Suzuki", "Gladius", "biało-niebieski", "naked", 2016, 4, 650);
 
 insert into motorcycle
 	(brand, model, color, type, production_year, cylinders, engine_capacity)
-values 
+values
 	("Romet", "ZXT125", "zielony", "naked", 2020, 4, 125);
 
 
 insert into user (username, password, first_name, last_name, email, phone_number, balance)
 values
 ('jan', '$2y$12$TsEAMCX.eWcfRR6R1NHMDenK.q4dyM3aAOdyIILE4dbWLuxpMkY7m', 'Jan', 'Adamski', 'jan@company.com', '123456789', 500),
-('maria', '$2y$12$iFoZb5xxjNIVBNfyq4RkCeJ4bWoPd6RpscrwOOFtJ.o6V82TqyrOS', 'Maria', 'Curie', 'maria@company.com', '987654321', 10000),
-('sandra', '$2y$12$XAmI9mYySg8Z7xsIGCuXouyTQpC00iDfAdCYCAf.Bal0C1YSsQAU6', 'Sandra', 'Malczyk', 'sandra@company.com', '123987456', 65000),
-('piotr', '$2y$12$FhERKd4oTbb3YR8lIi3TU.ACU35dDEmz.zoOKAzmngdz9ZnTnQNje', 'Piotr', 'Kucharek', 'piotr@company.com', '789321456', 50000);
+('anna', '$2y$12$6FWT/Bm.bBBDzUodXmmi.uaZXijwD/U5ViiBU4RfB9h84U9lE4mCW', 'Anna', 'Nowak', 'anna@organizacja.com', '456123789', 10000),
+('tomek', '$2y$12$xO2mraOyGwhJjMa1hNNQhe.CMYNF6WdzcTTYdkPqIECisTtFRjNtu', 'Tomek', 'Kowalczyk', 'tomek@firma.com', '789123456', 15000),
+('karolina', '$2y$12$WHt5L/kmSmTiqFrbOVxpAeH0WjkZp1oFBqBhXt9BzLuRyP5GKfJ4S', 'Karolina', 'Wiśniewska', 'karolina@praca.com', '654321789', 25000);
 
 
 insert into role (name)
