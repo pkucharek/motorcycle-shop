83eb507f28897655ee387086a4d27cfb041628af
pkucharek <--global>
2020-11-10 18:36:11 +0100

    Proved that user cant buy offer if has enough money but is the owner

--- src/test/java/com/kucharek/motorcycleshop/data/UserTests.java
+++ src/test/java/com/kucharek/motorcycleshop/data/UserTests.java
@@ -24,4 +24,20 @@ public class UserTests {
         assertThat(canBuy).isTrue();
     }
 
+    @Test
+    void user_cant_buy_offer_if_has_enough_money_but_is_the_owner() {
+        User seller = User.builder()
+                .balance(1000L).build();
+
+        Offer offer = Offer.builder()
+                .owner(seller)
+                .motorcycle(new Motorcycle())
+                .price(500L)
+                .build();
+
+        boolean canBuy = seller.canBuyMotorcycle(offer);
+
+        assertThat(canBuy).isFalse();
+    }
+
 }
