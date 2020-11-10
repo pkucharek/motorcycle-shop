a3c689c7e95bd12838b243b1decd714686c98197
pkucharek <--global>
2020-11-10 18:35:34 +0100

    Refactored user tests to use builder

--- src/test/java/com/kucharek/motorcycleshop/data/UserTests.java
+++ src/test/java/com/kucharek/motorcycleshop/data/UserTests.java
@@ -9,13 +9,15 @@ public class UserTests {
     @Test
     void user_can_buy_offer_if_is_not_the_owner_and_has_enough_money() {
         User seller = new User();
-        User buyer = new User();
-        buyer.setBalance(1000L);
+        User buyer = User.builder()
+                .balance(1000L)
+                .build();
 
-        Offer offer = new Offer();
-        offer.setOwner(seller);
-        offer.setMotorcycle(new Motorcycle());
-        offer.setPrice(500L);
+        Offer offer = Offer.builder()
+                .owner(seller)
+                .motorcycle(new Motorcycle())
+                .price(500L)
+                .build();
 
         boolean canBuy = buyer.canBuyMotorcycle(offer);
 
